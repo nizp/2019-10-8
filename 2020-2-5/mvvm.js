3f22f8ae092ad1614aa65aff144e28cf5a65efee
nizp <nzp890524@163.com>
2020-02-05 12:00:09 +0800

    2020-2-5上午

---
+++ 2020-2-5/mvvm.js
@@ -0,0 +1,177 @@
+class Vue {
+    constructor(opt){
+        this.$el = opt.el;
+        this.$data = opt.data;
+        //专门就是来编译模板的
+        if(this.$el){
+            new Obsever(this.$data);
+            new Complier(this.$el,this);
+        }
+    }
+}
+//编译器(把数据挂在DOM中)
+class Complier {
+    constructor(el,vm){
+        this.el = this.isElementNode(el)?el:document.querySelector(el);
+        this.vm = vm;
+        // console.log(this.el);
+        //创建一个文档碎片（打算把DOM中的节点都放到内存中操作,提高性能）
+        let frag = this.fargmentNode(this.el);
+
+        //处理文档碎片中的属性
+        this.complie(frag);
+
+        //把编译好的内容插入到页面
+        this.el.appendChild(frag);
+    }
+
+    //判断是否为元素节点
+    isElementNode(node){
+        return node.nodeType === 1;
+    }
+    fargmentNode(node){
+        //创建文档碎片
+        let frag = document.createDocumentFragment();
+        let firstChild;
+        //把app中的第一个娃剪切出来放到文档碎片中
+        while(firstChild = node.firstChild){
+            frag.appendChild(firstChild)
+        }
+        return frag;
+    }
+    complie(frag){
+        //从文档碎片中获取到子节点
+        const nodes = [...frag.childNodes];
+        //循环子节点查看是否有v-开头的或者{{}}
+        nodes.forEach(node=>{
+            if(this.isElementNode(node)){
+                //找到元素节点的属性
+                let attrs = [...node.attributes];
+                attrs.forEach(attr=>{
+                    // /^v-/.test(attr.nodeName)
+                    //看头是否为v-的属性，如果是就把数据中的值取出来赋值给元素的属性
+                    if(attr.nodeName.startsWith('v-')){
+                        let {nodeValue} = attr;
+                        //value就是data中的数据
+                        new Watcher(this.vm,nodeValue,(newVal)=>{
+                            node.value = newVal; //把最新的数据赋值给input的value
+                        });
+                        let value = this.vm.$data[nodeValue];
+                        node.oninput = (ev)=>{
+                            this.vm.$data[nodeValue] = ev.target.value; //set
+                            // console.log(this.vm.$data[nodeValue])
+                        }
+                        node.value = value;
+                    }
+                });
+            }else{
+                if(/\{\{(\w+)\}\}/.test(node.nodeValue)){
+                    // console.log(node)
+                    let str = node.nodeValue,
+                    key;
+                    let attr = str.replace(/\{\{(\w+)\}\}/,(...arg)=>{
+                        // console.log(arg)
+                        key = arg[1];
+                        return this.vm.$data[arg[1]];
+                    });
+
+                    new Watcher(this.vm,key,(newVal)=>{
+                        node.nodeValue = newVal; //把最新的数据赋值给有小胡子的{{}}文本
+                    });
+
+                    node.nodeValue = attr;
+                }
+            }
+        });
+        // console.log(nodes);
+    }
+}
+
+
+
+//发布订阅器
+class Dep {
+    constructor(){
+        this.sub = [];//存放Watcher的池子
+    }
+    //订阅
+    addSub(watcher){
+        this.sub.push(watcher);
+    }
+    //发布
+    notify(){
+        this.sub.forEach(watcher=>{
+            watcher.update();
+        });
+    }
+}
+
+
+//观察者模式，通过监控当前值的变化来关联视图
+
+//当新值和老值不一样的时候执行回调函数
+class Watcher {
+    //vm -> vm.$data   key -> 监控的数据  cb -> 数据变化之后的回调
+    constructor(vm,key,cb){
+        //当实例化Watcher的时候把实例挂到Dep的属性下
+        Dep.target = this; //let target = this
+        this.vm = vm;
+        this.key = key;
+        this.cb = cb;
+        this.oldVal = this.get();
+    }
+    get(){
+        let val = this.vm.$data[this.key];
+        return val;
+    }
+    update(){
+        let newVal = this.get(); //设置的时候才拿得到新值
+        if(newVal !== this.oldVal){
+            this.cb(newVal)
+        }
+    }
+}
+
+
+
+//专门用来进行数据劫持的（把data中的所有数据都进行一个数据劫持）
+class Obsever {
+    constructor(data){
+        this.obsever(data);
+    }
+    //循环对象，然后把进行数据劫持操作
+    obsever(data){
+        if(data && Object.prototype.toString.call(data) === '[object Object]'){
+            for(let key in data){
+                this.defineReactive(data,key,data[key]);
+            }
+        }
+    }
+    //数据劫持
+    defineReactive(obj,key,value){
+        //如果对象的值还是为对象，那么进行深度递归（目的就是为了让引用类型有数据劫持的特性）
+        if(typeof value === 'object'){
+            this.obsever(value);
+        }
+        let dep = new Dep();
+        Object.defineProperty(obj,key,{
+            get(){
+                //在获取数据的时候进行订阅
+                dep.addSub(Dep.target);
+                return value;
+            },
+            set:(newVal)=>{
+                //在设置value值的时候如果新值和老值不相等就更新数据
+                if(value !== newVal){
+                    this.obsever(newVal); //保证新赋值的数据也被数据劫持了
+                    value = newVal;
+                    //在数据发生变化的时候进行watcher发布
+                    dep.notify();
+                    // console.log(111)
+                }
+            }
+        })
+    }
+}
+
+
